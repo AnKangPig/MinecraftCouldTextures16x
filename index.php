af9e6d1557b70a32ab64363da7a32ccc06ea4dbc
AnKangPig <Ankangpig@qq.com>
2022-06-22 10:08:52 +0800

    同步

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<span style="font-family:Arial;"><!DOCTYPE html>
+<html>
+<head>
+    <!-- 请将文档保存为UTF-8编码，否则中文会乱码 -->
+    <meta charset="UTF-8">
+    <title>案例2</title><!-- PHP绘制图片、绘制中文 -->
+</head>
+ 
+<body>
+ 
+<?php
+$has = extension_loaded('GD');
+var_dump($has);
+?> 
+</body></span><span style="font-family:Times New Roman;">
+ 
+</html></span>
+
